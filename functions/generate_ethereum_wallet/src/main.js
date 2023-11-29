0a98b7cbbf5bde75334d25ed75499c0d975b4cb0
Dan Zulla <114028070+zdanl@users.noreply.github.com>
2023-11-29 20:56:34 +0100

    Update main.js

--- functions/generate_ethereum_wallet/src/main.js
+++ functions/generate_ethereum_wallet/src/main.js
@@ -1,33 +1,39 @@
 import { Client } from 'node-appwrite';
+const ethers = require('ethers');
 
-// This is your Appwrite function
-// It's executed each time we get a request
+async function createWallet() {
+  // Generate a new random wallet
+  const wallet = ethers.Wallet.createRandom();
+  return wallet;
+  
+  // Get the private key and mnemonic (seed phrase)
+  const privateKey = wallet.privateKey;
+  const mnemonic = wallet.mnemonic.phrase;
+
+  console.log('Address:', wallet.address);
+  console.log('Private Key:', privateKey);
+  console.log('Mnemonic (Seed Phrase):', mnemonic);
+}
+
+// This is the Ethers.js Appwrite function
+// It's executed each time a user is created or upon request
 export default async ({ req, res, log, error }) => {
-  // Why not try the Appwrite SDK?
+  // Invoking the Appwrite SDK?
   //
-  // const client = new Client()
-  //    .setEndpoint('https://cloud.appwrite.io/v1')
-  //    .setProject(process.env.APPWRITE_FUNCTION_PROJECT_ID)
-  //    .setKey(process.env.APPWRITE_API_KEY);
+  const client = new Client()
+    .setEndpoint('https://cloud.appwrite.io/v1')
+    .setProject('lyra')
+    .setKey(process.env.APPWRITE_API_KEY);
 
   // You can log messages to the console
-  log('Hello, Logs!');
-
-  // If something goes wrong, log an error
-  error('Hello, Errors!');
-
-  // The `req` object contains the request data
-  if (req.method === 'GET') {
-    // Send a response with the res object helpers
-    // `res.send()` dispatches a string back to the client
-    return res.send('Hello, World!');
-  }
+  log('Creating Ethereum Wallet for User ...');
+  const wallet = createWallet();
+  
+  log(`Created Wallet ${wallet.address}`);
 
   // `res.json()` is a handy helper for sending JSON
   return res.json({
-    motto: 'Build like a team of hundreds_',
-    learn: 'https://appwrite.io/docs',
-    connect: 'https://appwrite.io/discord',
-    getInspired: 'https://builtwith.appwrite.io',
+    address: 'Build like a team of hundreds_',
+    timestamp: new Date().getTime()
   });
 };
