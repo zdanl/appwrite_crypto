3c912af343ad06c91c1ccf1427267d117ea23e56
root <root@exchange.lyra.ly>
2023-11-29 20:09:43 +0000

    added ethers dependency

--- functions/generate_ethereum_wallet/src/main.js
+++ functions/generate_ethereum_wallet/src/main.js
@@ -1,25 +1,10 @@
 import { Client } from 'node-appwrite';
 import { ethers } from 'ethers';
 
-async function createWallet() {
-  // Generate a new random wallet
-  const wallet = ethers.Wallet.createRandom();
-  
-  // Get the private key and mnemonic (seed phrase)
-  const privateKey = wallet.privateKey;
-  const mnemonic = wallet.mnemonic.phrase;
-
-  console.log('Address:', wallet.address);
-  console.log('Private Key:', privateKey);
-  console.log('Mnemonic (Seed Phrase):', mnemonic);
-  return wallet;
-}
-
 // This is the Ethers.js Appwrite function
 // It's executed each time a user is created or upon request
 export default async ({ req, res, log, error }) => {
   // Invoking the Appwrite SDK?
-  //
   const client = new Client()
     .setEndpoint('https://cloud.appwrite.io/v1')
     .setProject('lyra')
@@ -27,7 +12,15 @@ export default async ({ req, res, log, error }) => {
 
   // You can log messages to the console
   log('Creating Ethereum Wallet for User ...');
-  const wallet = createWallet();
+  const wallet = ethers.Wallet.createRandom();
+   
+  // Get the private key and mnemonic (seed phrase)
+  const privateKey = wallet.privateKey;
+  const mnemonic = wallet.mnemonic.phrase;
+
+  console.log('Address:', wallet.address);
+  console.log('Private Key:', privateKey);
+  console.log('Mnemonic (Seed Phrase):', mnemonic);
   
   log(`Created Wallet ${wallet.address}`);
 
